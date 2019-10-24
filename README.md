17dfd40554f81a4d3c286c9b7ca1c6503b2a2097
noemi alonso <noemialonso@MBP-de-noemi.lan>
2019-10-24 14:45:06 +0200

    fist commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# words
+
+## Description
+
+Página/aplicación donde un usuario se puede registrar, logear y editar su información de perfil.
+El usuario podrá crear un anima, con unas características predefinidas y otras editables y también podrá borrar su animal.
+Finalmente el usuario se enfrentará a una batalla con el animal de otro usuario.
+
+## MVP
+
+>>routas, 
+home pública, sign up y login
+Sign up, formulario para creación de usuario
+Log in, find de DB
+perfil de User, Información de perfil del User
+User/:id/edit, Edición de la información del perfil
+animal/:id/create,  Creación de animal
+animal/:id/edit,  Edicion del animal
+home privada, Home visible para usuarios logrados
+game/:id,  Interacción con el juego
+game/:id/winner, pagina resultado
+game/:id/loser , pagina resultado
+
+>>Modelos:
+Usuario
+Animal
+Pregunta
+
+
+## Backlog
+Añadir otro tipo de animal
+Añadir preguntas
+Añadir comportamiento del animal
+Y muy futuro, dependiendo de las victorias y derrotas subida de experiencia del anima
+Modelo Game
+
+## Data structure
+Classes and methods definition.
+
+
+## States y States Transitions
+Definition of the different states and their transition (transition functions)
+
+
+
+## Task
+Backend
+Estructura de modelos, estructura de formularios
+Session
+Validaciones
+Html 
+Css 
+
+
+
+## Links
+
+### Trello
+[Link url](https://trello.com/b/noq1yTYS/dramas)
+
+
+### Git
+URls for the project repo and deploy
+[Link Repo](http://github.com)
+[Link Deploy](http://github.com)
+
+
+### Slides
+URls for the project presentation (slides)
+[Link Slides.com](http://slides.com)
---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/database-name', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  httpOnly: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+const hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
