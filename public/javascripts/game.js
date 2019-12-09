dc5809e4d8728ebf796cf9e84ae01ea9a70f0acf
nalonso404 <54322993+nalonso404@users.noreply.github.com>
2019-12-09 09:22:45 +0100

    Update game.js

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,29 +1,3 @@
-/* function pregunta (event){
-   const respuestas =  document.querySelectorAll('.respuesta')
-   console.log(respuestas)
-    respuestas.forEach(respuesta => {
-        respuesta.addEventListener('click', async (event) => {
-            console.log('hola')
-            //mirar las clases k tiene (classList)
-            const elementClasses = respuesta.classList;
-            console.log(elementClasses)
-            setTimeout(async function(){
-                if(elementClasses[0] === 'respuesta-correcta'){
-                    console.log("win")
-                    await axios.post('/users/winner');
-                    window.location.href = '/users/winner';
-                }else {
-                    console.log("loseeeer")
-                    await axios.post('/users/loser');
-                    window.location.href = '/users/loser';
-                }
-              }, 10000);
-            
-        })
-    });
-} */
-
-
 function game() {
     const respuestaCorrecta = document.querySelector('.respuesta-correcta')
     console.log(respuestaCorrecta);
